28095e6964b65d764aef417820d96197067ae1e1
Hajavololona Armellin <armelgeek5@gmail.com>
2024-01-31 12:02:20 +0300

    ajout de quelque capture pour tiakalo web

--- src/components/projects/TiakaloWeb.jsx
+++ src/components/projects/TiakaloWeb.jsx
@@ -6,6 +6,11 @@ import tk3 from '../../assets/img/tiakalo-web/3.png'
 import tk4 from '../../assets/img/tiakalo-web/4.png'
 import tk5 from '../../assets/img/tiakalo-web/5.png'
 import tk6 from '../../assets/img/tiakalo-web/6.png'
+import tk7 from '../../assets/img/tiakalo-web/7.png'
+import tk8 from '../../assets/img/tiakalo-web/8.png'
+import tk9 from '../../assets/img/tiakalo-web/9.png'
+import tk10 from '../../assets/img/tiakalo-web/10.png'
+import tk11 from '../../assets/img/tiakalo-web/11.png'
 const TiakaloWeb = memo(() => {
     const responsiveSettings = [
         {
@@ -74,7 +79,46 @@ const TiakaloWeb = memo(() => {
                     backgroundRepeat: 'no-repeat'
                 }}></div>
             </div>
-           
+            <div className="img">
+                <div className="h-96" style={{
+                    backgroundImage: `url(${tk7})`,
+                    backgroundPosition: 'center',
+                    backgroundSize: 'contain',
+                    backgroundRepeat: 'no-repeat'
+                }}></div>
+            </div>
+            <div className="img">
+                <div className="h-96" style={{
+                    backgroundImage: `url(${tk8})`,
+                    backgroundPosition: 'center',
+                    backgroundSize: 'contain',
+                    backgroundRepeat: 'no-repeat'
+                }}></div>
+            </div>
+            <div className="img">
+                <div className="h-96" style={{
+                    backgroundImage: `url(${tk9})`,
+                    backgroundPosition: 'center',
+                    backgroundSize: 'contain',
+                    backgroundRepeat: 'no-repeat'
+                }}></div>
+            </div>
+            <div className="img">
+                <div className="h-96" style={{
+                    backgroundImage: `url(${tk10})`,
+                    backgroundPosition: 'center',
+                    backgroundSize: 'contain',
+                    backgroundRepeat: 'no-repeat'
+                }}></div>
+            </div>
+            <div className="img">
+                <div className="h-96" style={{
+                    backgroundImage: `url(${tk11})`,
+                    backgroundPosition: 'center',
+                    backgroundSize: 'contain',
+                    backgroundRepeat: 'no-repeat'
+                }}></div>
+            </div>
            
         </Slide>
     )
