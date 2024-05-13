61e4324f8ff5612d6c3957f19dcde33cb46a5445
armelgeek <57889510+armelgeek@users.noreply.github.com>
2024-05-13 15:40:13 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -301,6 +301,47 @@ function App() {
             Experiences profesionnelles
           </h3>
           <div className="space-y-8 relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px md:before:mx-auto md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent">
+            
+          <div className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
+              <div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-purple-500 text-slate-500 group-[.is-active]:text-purple-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
+                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="12" height="10">
+                  <path fillRule="nonzero" d="M10.422 1.257 4.655 7.025 2.553 4.923A.916.916 0 0 0 1.257 6.22l2.75 2.75a.916.916 0 0 0 1.296 0l6.415-6.416a.916.916 0 0 0-1.296-1.296Z" />
+                </svg>
+              </div>
+              <div className="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow">
+                <div className="flex items-center justify-end space-x-2 mb-1">
+
+                  <time className="font-medium text-purple-500" style={{
+                    fontFamily: 'Karla'
+                  }}>09 Mars 2022 -  10 Septembre 2022</time>
+                </div>
+                <div className="font-bold text-slate-900 text-lg  mb-3">Developpeur FullStack chez Blue Cactus Factory</div>
+
+                <div className="text-slate-500" style={{
+                  fontFamily: 'Karla'
+                }}>
+                  <div className='flex flex-col flex-wrap gap-2'>
+                    <div className="flex flex-row">
+                      <div>
+                        <div className="w-3 h-3 bg-purple-500 rounded-full mt-2 mr-3">
+
+                        </div>
+                      </div>
+                      <p>Mis en  place d’une plateforme de cours en ligne nommé « Schoolathome »</p>
+                    </div>
+                    <div className="flex flex-row flex-wrap">
+                      <div>
+                        <div className="w-3 h-3 bg-purple-500 rounded-full mt-2 mr-3">
+
+                        </div>
+                      </div>
+                      <p >Conception et réalisation d’un site d’un hôtel française  nommé « Loup blanc »</p>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            </div>
             <div className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
               <div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-purple-500 text-slate-500 group-[.is-active]:text-purple-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
                 <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="12" height="10">
