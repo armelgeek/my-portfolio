ac029126f06c8d468c3720f346ed906406d54f6e
Hajavololona Armellin <57889510+armelgeek@users.noreply.github.com>
2026-08-09 18:11:27 +0300

    Render experiences from data; update CV files
    Replace the static timeline entry in App.jsx with a mapped list from data/experiences (adds import and uses experiences.map with key={exp.id}). Remove the old .docx CV and replace the PDF file in public/cv. Changes keep the timeline styling while making entries data-driven for easier maintenance.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { FaArrowRight, FaCircleArrowRight, FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa6";
 import './App.css'
 import SimpleProjectsSection from "./components/SimpleProjectsSection";
+import { experiences } from "./data/experiences";
 
 function calculateYearDifference() {
   const currentYear = new Date().getFullYear();
@@ -110,8 +111,8 @@ function App() {
           </h3>
           <div
               className="space-y-8 relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px md:before:mx-auto md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent">
-
-            <div
+            {experiences.map((exp) => (
+              <div key={exp.id}
                 className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
               <div
                   className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-purple-500 text-slate-500 group-[.is-active]:text-purple-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
